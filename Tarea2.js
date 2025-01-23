6110efa2f3413632c645beac550515b0855922db
Priscilla Azofeifa <92341984+PriA03@users.noreply.github.com>
2025-01-22 19:38:58 -0600

    Add files via upload

---
+++ Tarea2.js
@@ -0,0 +1,106 @@
+function addEvents (){
+
+    let add = prompt('Indique la cantidad de eventos desea agregar');
+
+    let eventsList = [];
+
+    let eventName;
+    let eventRoom;
+    let eventSeats;
+    let eventDate;
+
+    for (let i = 0; i<add; i++) {
+
+        eventName = prompt('Indique el nombre del evento');
+        eventRoom = parseInt(prompt('Indique el número de sala'), 10);
+        eventSeats = parseInt(prompt('Indique la cantidad de espacios disponibles'), 10);
+        eventDate = prompt('Indique la fecha y hora del evento. Ejm: 02/02/25 - 5:30 pm');
+        
+        let eventData = {
+            id: i,
+            name: eventName,
+            room: eventRoom,
+            seats: eventSeats,
+            date: eventDate
+        };
+
+        eventsList.push(eventData);
+    }
+
+
+    return eventsList;
+}
+
+
+
+
+function viewEvents(eventsList){
+
+    for (let i = 0; i < eventsList.length; i++) {
+
+        let list = eventsList[i];
+
+        console.log(`Bienvenid@ al Teatro Sueños de Colores \n\nEvento: ${list.name} \nSala: ${list.room} \nAsientos disponibles: ${list.seats} \nFecha: ${list.date}`);
+    }
+}
+
+
+function buyTickets(eventsList, eventDesired){
+
+    let found = false;
+    const attemptsAccepted = 3;
+    const attemptsFailed = 0;
+
+    for (let i = 0; i < eventsList.length; i++) {
+
+    if (eventsList[i] === eventDesired) {
+
+        found = true;
+
+    }else {
+
+        eventDesired = prompt('El evento no existe, vuelva a ingresar el nombre del evento, por favor');
+
+        if (attemptsFailed === attemptsAccepted){
+            console.log('Lo sentimos, ha escrito de manera erronea el evento demasiadas veces. Por favor inténtelo más tarde');
+            break;
+        }
+
+    }
+
+    }
+
+    if (found != false){
+
+        let seatsRequired = parseInt(prompt('Indique cuántos asientos desea comprar'), 10);
+
+        for (let i = 0; i < eventsList.length; i++) {
+
+            if (eventsList[i] === eventDesired) {
+
+                eventList.seats = eventList.seats - seatsRequired;
+                break;
+        
+            }
+        }
+
+    }
+
+    return eventList;
+
+}
+
+let eventsCreated = addEvents();
+viewEvents(eventsCreated);
+
+let buy = prompt('Indique el nombre del evento al que desea asistir');
+
+switch (buy){
+
+}
+let eventDesired = prompt('Indique el nombre del evento al que desea asistir');
+
+let reloadEvents = buyTickets()
+
+
+
