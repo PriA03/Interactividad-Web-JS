67bf47ebb62725617a4932f0016f3ff48ec33b30
Priscilla Azofeifa <92341984+PriA03@users.noreply.github.com>
2025-01-22 20:50:32 -0600

    Final changes

--- Tarea2.js
+++ Tarea2.js
@@ -36,59 +36,16 @@ function addEvents (){
 
 function viewEvents(eventsList){
 
+    console.log('Bienvenid@ al Teatro Sueños de Colores ');
+
     for (let i = 0; i < eventsList.length; i++) {
 
         let list = eventsList[i];
-
-        console.log(`Bienvenid@ al Teatro Sueños de Colores \n\nEvento: ${list.name} \nSala: ${list.room} \nAsientos disponibles: ${list.seats} \nFecha: ${list.date}`);
+        console.log(`\nEvento: ${list.name} \nSala: ${list.room} \nAsientos disponibles: ${list.seats} \nFecha: ${list.date}`);
     }
 }
 
 
-function buyTickets(eventsList, eventDesired){
-
-    let found = false;
-    const attemptsAccepted = 3;
-    const attemptsFailed = 0;
-
-    for (let i = 0; i < eventsList.length; i++) {
-
-    if (eventsList[i] === eventDesired) {
-
-        found = true;
-
-    }else {
-
-        eventDesired = prompt('El evento no existe, vuelva a ingresar el nombre del evento, por favor');
-
-        if (attemptsFailed === attemptsAccepted){
-            console.log('Lo sentimos, ha escrito de manera erronea el evento demasiadas veces. Por favor inténtelo más tarde');
-            break;
-        }
-
-    }
-
-    }
-
-    if (found != false){
-
-        let seatsRequired = parseInt(prompt('Indique cuántos asientos desea comprar'), 10);
-
-        for (let i = 0; i < eventsList.length; i++) {
-
-            if (eventsList[i] === eventDesired) {
-
-                eventList.seats = eventList.seats - seatsRequired;
-                break;
-        
-            }
-        }
-
-    }
-
-    return eventList;
-
-}
 
 
 let eventsCreated = addEvents();
@@ -97,25 +54,7 @@ let allEvents = viewEvents(eventsCreated);
 
 
 
-let buy = prompt('Desea comprar tiquetes?. Ingrese: 1 (Sí) o 2 (No)');
 
-switch (buy){
-
-    case 1: 
-    
-    allEvents;
-    let eventDesired = prompt('Indique el nombre del evento al que desea asistir. Debe escribirlo tal y como se mostraba en la lista');
-    let reloadEvents = buyTickets(eventDesired, allEvents);
-
-    case 2: 
-    
-    console.log('Muchas gracias por visitarnos!');
-    break;
-
-    default:
-    console.log('Has elegido una opción no existente');
-
-}
 
 
 
